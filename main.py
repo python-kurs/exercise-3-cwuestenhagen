0055e220a99ef1c53f88eb6f870b9ca88b41a928
cwuestenhagen <33098140+cwuestenhagen@users.noreply.github.com>
2019-05-28 11:29:16 +0200

    final_version

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 # Exercise 3
 from pathlib import Path
+import utils as ut
+
 # import functions from utils here
 
-data_dir = Path("data")
-output_dir = Path("solution")
+main_dir = Path("C:/Users/coral/Documents/GitHub/exercise-3-cwuestenhagen/")
+data_dir = main_dir / "data"
+output_dir = main_dir / "solution"
 
 # 1. Contstruct the path to the text file in the data directory using the `pathlib` module [2P]
 
+car_path = data_dir / "cars.txt"
 
 # 2. Read the text file [2P]
 
+with open(car_path, "r") as file:
+    lines = file.read().splitlines()
+print (lines)
+
 
 # 3. Count the occurences of each item in the text file [2P]
-
+        
+ut.countValues(lines)
 
 # 4. Using `pathlib` check if a directory with name `solution` exists and if not create it [2P]
 
+ut.createDir(output_dir)
 
 # 5. Write the counts to the file `counts.csv` in the `solution` directory in the format (first line is the header): [2P]
-#    item, count
-#    item_name_1, item_count_1
-#    item_name_2, item_count_2
-#    ...
+
+ut.CountandCreate(lines, output_dir)
--- utils.py
+++ utils.py
@@ -2,3 +2,62 @@
 
 # Use this file for all functions you create and might want to reuse later.
 # Import them in the `main.py` script
+
+def countValues(x):
+    """
+    Count the sum of each item in the list.
+    
+    Parameter: 
+    x : list or any readable in python
+    
+    Returns:
+    Total sum for each item in the list. 
+    
+    """
+    items = list(set(x))
+    for value in items:
+        print(value, x.count(value))
+        
+def createDir(x):
+    """
+    Creates a subdirectory
+    
+    Parameter:
+    x : subfolder to be created
+    
+    Returns: 
+    Prints a message if the subfolder exists. If the message is false, a new subfolder will be created. 
+    
+    """
+   sub_dir = x.exists()
+   if sub_dir:
+       print("The subfolder {} exists.".format(x))
+      
+   else:
+       x.mkdir(parents = True, exist_ok = True)
+       print("The subfolder {} is created.".format(x))
+
+    
+def CountandCreate(x, output_dir):
+    """
+    Creation of a dataframe with the sum of each item i a text file
+    
+    Parameter: 
+    x: dataframe to be created
+    
+    Returns:
+    Saves the dataframe in a csv-file.
+    
+    """
+    import pandas as pd
+    items = list(set(x))
+    countings = []
+    for value in items: 
+        countings.append(x.count(value))
+    countings_df = pd.DataFrame({'model': items, 'countings': countings})
+    
+    output_name = output / "counts.csv"      
+    countings_df.to_csv (output_name, header = True)
+
+        
+
